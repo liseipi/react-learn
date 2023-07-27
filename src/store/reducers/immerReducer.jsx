8efe12c5a4a087f946fee2395e0815698099a0d0
huiliu <hui@qq.com>
2023-07-27 15:17:45 +0800

    add immer reducer 数据持久

--- src/store/reducers/immerReducer.jsx
+++ src/store/reducers/immerReducer.jsx
@@ -5,6 +5,15 @@ const initState = {
     originList: []
 }
 
+const getLocalstorageData = (initState) => {
+    const data = localStorage.getItem('immerData');
+    return !!data ? JSON.parse(data) : initState
+}
+
+const setLocalstorageData = (data) => {
+    localStorage.setItem('immerData', JSON.stringify(data))
+}
+
 const actions = {
     addList(draft, value) {
         draft.list.push(value.item)
@@ -17,12 +26,17 @@ const actions = {
     }
 }
 
-export const ImmerTestReducer = produce((draft = initState, action) => {
+export const ImmerTestReducer = produce((draft = getLocalstorageData(initState), action) => {
     if (actions[action.type]) {
         actions[action.type](draft, action)
+
+        localStorage.setItem('immerData', JSON.stringify(draft))
+
+        setLocalstorageData(draft)
         return draft
     }
 
+    setLocalstorageData(draft)
     return draft
 })
 
